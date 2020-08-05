c9ee64a0d22999c7f88fa98fba3974ad609aca25
alexmaar <oo.marzec@gmail.com>
2020-08-05 14:22:09 +0200

    next endpoints added

--- src/rick_and_morty_api/core.clj
+++ src/rick_and_morty_api/core.clj
@@ -5,35 +5,41 @@
             [clojure.data.json :as json]
             [ring.adapter.jetty :refer [run-jetty]]))
 
-(def characters (ref {}))
-(def users (ref {}))
+(def characters (ref {})) ;map of characters which were searched
+(def users (ref {})) ;map of users and amout of their requests
 
+(defn get-by-number [number]
+  (if (<= 0 (Integer/parseInt number) 591)
+    (if-let [character (get @characters number)]
+      (merge {:success true :source "db"} character)
+      (let [body (:body (client/get (str "https://rickandmortyapi.com/api/character/" number)))
+            content (json/read-str body :key-fn keyword)
+           {name :name episode :episode} content
+           character {number {:name name :episode episode}}]
+        (dosync (alter characters conj character))
+        (merge {:success true :source "api"} character)))
+   {:success false :error-message "wrong number"}))
 
-(defn get-by-number [number body]
-  (if-let [character (get @characters number)]
-    (str "Character's name from data-base: " (:name character) ". \nEpisodes: " (:episode character) ". ")
-    (let [b (json/read-str (body (client/get (str "https://rickandmortyapi.com/api/character/" number))))
-        name (get b "name")
-        episode (get b "episode")]
-      (dosync (alter characters conj {number {:name name :episode episode}}))
-      (str "Character's name: " name ". \nEpisodes: " episode ". "))))
+(defn get-by-number-and-nick [number nick]
+  (dosync
+   (if (get @users nick)
+    (alter users update nick inc)
+    (alter users conj {nick 1})))
+  (get-by-number number))
 
-(defn get-by-number-and-nick [number nick body]
-  (if-let [req-no (get @users nick)]
-    (dosync 
-     (alter users dissoc nick)
-     (alter users conj {nick (inc req-no)})
-    (dosync (alter users conj {nick 1})))
-  (get-by-number number body)))
-
+(defn requests-number [nick]
+  (if-let [requests-no (get @users nick)]
+    {:success true :result requests-no}
+    {:success false :error-message "user does not exists"}))
 
 (def app
   (api
-   (GET "/number" [number]
-     (ok  (get-by-number number :body)))
-   (GET "/number-and-nick" [number nick]
-     (ok (get-by-number-and-nick number nick :body)))))
-
+   (GET "/character" [number]
+     (ok  (get-by-number number)))
+   (GET "/character-and-nick" [number nick]
+     (ok (get-by-number-and-nick number nick)))
+   (GET "/requests-number" [nick]
+     (ok (requests-number nick)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
